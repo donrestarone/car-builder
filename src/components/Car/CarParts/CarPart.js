a7f86a867b725a03ffabcbc2c7a6148116f7a97e
donrestarone <shashikejayatunge@gmail.com>
2019-01-31 07:49:35 -0500

    add proptypes

--- src/components/Car/CarParts/CarPart.js
+++ src/components/Car/CarParts/CarPart.js
@@ -1,18 +1,25 @@
-import React from 'react'
+import React, {Component} from 'react'
+import PropTypes from 'prop-types'
+
 let sedan = require('../../../assets/cars/icons8-sedan-96.png')
 let sedan1 = require('../../../assets/cars/icons8-car-96.png')
 
-const carPart = (props) => {
-  let part = null
-  switch (props.type) {
-    case('sedan'):
-      part = <img src={sedan} />
-      break;
-    case('sedan-1'):
-      part = <img src={sedan1}></img>
-      break;
+class CarPart extends Component {
+  render () {
+    let part = null
+    switch (this.props.type) {
+      case('sedan'):
+        part = <img src={sedan} />
+        break;
+      case('sedan-1'):
+        part = <img src={sedan1}></img>
+        break;
+    }
+    return part
   }
-  return part
 }
 
-export default carPart
+CarPart.propTypes = {
+  type: PropTypes.string.isRequired
+}
+export default CarPart
